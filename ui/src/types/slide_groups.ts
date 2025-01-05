966f262076d3463e8b2237d69f81a34681251273
Abe Gong <abegong@gmail.com>
2025-01-05 08:30:48 -0800

    Decruft

--- ui/src/types/slide_groups.ts
+++ ui/src/types/slide_groups.ts
@@ -7,7 +7,7 @@
  * 3. SlideGroupConfigType - A type utility for type-safe discrimination of slide group types
  */
 
-import { SlideType, BulletSlideConfig, ChartSlideConfig, PictureSlideConfig } from './slides';
+import { SlideType, BulletSlideConfig, ChartSlideConfig } from './slides';
 import { ReactNode } from 'react';
 
 
