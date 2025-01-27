7b30c63ba2aeb3e9ca0b4e2a55e711cd5c18aae7
Abe Gong <abegong@gmail.com>
2025-01-27 11:11:48 -0700

    Oops, add colors.ts

---
+++ ui/src/theme/colors.ts
@@ -0,0 +1,16 @@
+/**
+ * Color palette used for slide transitions and visual styling.
+ * Each color has a hex value and a descriptive name.
+ */
+export const colors = [
+  { hex: '#FF6B6B', name: 'Coral Red' },
+  { hex: '#4ECDC4', name: 'Turquoise' },
+  { hex: '#45B7D1', name: 'Sky Blue' },
+  { hex: '#96CEB4', name: 'Sage Green' },
+  { hex: '#FFEEAD', name: 'Cream Yellow' },
+  { hex: '#D4A5A5', name: 'Dusty Rose' },
+  { hex: '#9B5DE5', name: 'Purple' },
+  { hex: '#00BBF9', name: 'Bright Blue' },
+  { hex: '#00F5D4', name: 'Mint' },
+  { hex: '#FEE440', name: 'Yellow' },
+];
